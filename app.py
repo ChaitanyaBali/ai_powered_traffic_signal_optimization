aa58c1a057ad0a419ebf9ed928a7c5c75937fc5e
ChaitanyaBali <chaitanyabali878@gmail.com>
2026-03-11 15:53:55 +0530

    app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from PIL import Image
 from vehicle_detection import detect_vehicles
 
 # Page settings
-st.set_page_config(page_title="AI Traffic Signal System", layout="wide")
+st.set_page_config(page_title="AI Traffic Signal Optimization", layout="wide")
 
 # Background color
 page_bg = """
@@ -21,22 +21,25 @@ st.title("🚦 AI Traffic Signal Optimization System")
 st.write("Real-time traffic monitoring using AI")
 
 # Upload image
-uploaded_file = st.file_uploader("Upload Traffic Image", type=["jpg","png","jpeg"])
+uploaded_file = st.file_uploader(
+    "Upload Traffic Image", type=["jpg", "jpeg", "png"]
+)
 
 if uploaded_file is not None:
 
     image = Image.open(uploaded_file)
     img = np.array(image)
 
+    # Detect vehicles
     result, total = detect_vehicles(img)
 
-    col1, col2 = st.columns([2,1])
+    col1, col2 = st.columns([2, 1])
 
     # Show detected image
     with col1:
         st.image(result, caption="Detected Vehicles", use_container_width=True)
 
-    # Traffic signal decision
+    # Signal decision
     with col2:
 
         st.subheader("Traffic Signal Decision")
@@ -44,9 +47,11 @@ if uploaded_file is not None:
         if total <= 5:
             signal = "🔴 RED"
             time = 60
+
         elif total <= 15:
             signal = "🟡 YELLOW"
             time = 30
+
         else:
             signal = "🟢 GREEN"
             time = 10
